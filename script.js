62cb3b3b98e93d7e07c6f33b6e8b6670b08fe312
Chinazom-1 <chinazom.ugwuanyi@gmail.com>
2024-11-05 23:23:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let greeting='Welcome To My Page';
+alert(greeting);
+
+console.log('Hello World', greeting);
+
+// function showPopup(){
+//     document.getElementById('heere').style.textDecoration = 'underlined'
+// }
